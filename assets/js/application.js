30968b886850e705633f26715c4dc07b6a2b0f5c
Lindsey Bieda <hazardous.narayan@gmail.com>
2020-07-17 12:44:30 -0400

    Closes #16
    Add filtering for gifs

--- assets/js/application.js
+++ assets/js/application.js
@@ -16,11 +16,13 @@
   );
 
   window.onload = function() {
+    window.currentClass = "";
     var links = document.getElementsByClassName("js-reveal");
     for(const link of links){
       link.onclick = function() {
         items = document.getElementsByClassName("grid-item");
         const klass = link.dataset.class;
+        window.currentClass = klass;
         for(const item of items) {
           if (item.classList.contains(klass)) {
             item.style.display = "inline";
@@ -28,7 +30,32 @@
             item.style.display = "none";
           }
         }
-        lazyLoadInstance.update();
+
+        if(window.lazyLoadInstance) {
+            window.lazyLoadInstance.update();
+        }
       }
     }
+
+    const filter = document.getElementById("filter");
+
+    filter.addEventListener('change', (event) => {
+        if(window.currentClass !== ""){
+            const filter = event.target.value;
+            const filters = filter.split(" ");
+            const regex = new RegExp(filters.join("|"), "i")
+            const items = document.getElementsByClassName(window.currentClass);
+            for(const item of items){
+                if(regex.test(item.src)){
+                    item.style.display = "inline";
+                } else {
+                    item.style.display = "none";
+                }
+            }
+        }
+    });
+
+    filter.addEventListener('focusin', (event) => {
+        event.target.value = "";
+    })
   }
